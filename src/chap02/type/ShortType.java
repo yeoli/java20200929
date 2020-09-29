6076c9c9c0dde130d0ea57935b976128dc9a6dcc
yeoli <yeo3580@naver.com>
2020-09-29 12:21:20 +0900

    short

---
+++ src/chap02/type/ShortType.java
@@ -0,0 +1,19 @@
+package chap02.type;
+
+public class ShortType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//short : 2byte;
+		short shortValue1;
+		shortValue1 = 32767;
+		System.out.println(shortValue1);
+		
+		//shortValue1 = 32768;
+		//shortValue1 = -32768;
+		System.out.println(shortValue1);
+
+	}
+
+}
